e477829f5c9fbdf8c719f7a9834f7b4408fc6e74
Nils Ove Tendenes <nils.ove@tendenes.com>
2025-08-27 15:56:54 +0200

    refactor: use builder when mapping

--- src/main/kotlin/no/digdir/fdk/parseservice/extract/dataset/Dataset.kt
+++ src/main/kotlin/no/digdir/fdk/parseservice/extract/dataset/Dataset.kt
@@ -9,26 +9,43 @@ import no.digdir.fdk.parseservice.extract.primaryTopicFromFdkRecord
 import no.digdir.fdk.parseservice.extract.singleObjectStatement
 import no.digdir.fdk.parseservice.namespace.FDK
 import org.apache.jena.rdf.model.Model
+import org.apache.jena.rdf.model.Resource
 import org.apache.jena.vocabulary.DCAT
 import org.apache.jena.vocabulary.DCTerms
 
 fun Model.extractDataset(): Dataset {
-    val builder = Dataset()
+    val builderV1 = Dataset.newBuilder()
+    //val builderV2 = Dataset.newBuilder()
+    //val builderV3 = Dataset.newBuilder()
 
-    val record = fdkRecord(listOf(DCAT.Dataset))
-    val dataset = record.primaryTopicFromFdkRecord(listOf(DCAT.Dataset))
-    val harvestMetaData = record.extractHarvestMetaData()
+    val recordResource = fdkRecord(listOf(DCAT.Dataset))
+    val datasetResource = recordResource.primaryTopicFromFdkRecord(listOf(DCAT.Dataset))
 
-    builder.id = record.singleObjectStatement(DCTerms.identifier)!!.string
-    builder.uri = dataset.uri
+    addCommonDatasetValuesToBuilder(recordResource, datasetResource, builderV1)
+    //addCommonDatasetValuesToBuilder(recordResource, datasetResource, builderV2)
+    //addCommonDatasetValuesToBuilder(recordResource, datasetResource, builderV3)
+
+    datasetResource.addV11ValuesToBuilder(builderV1)
+    //datasetResource.addV2ValuesToBuilder(builderV2)
+    //datasetResource.addV3ValuesToBuilder(builderV3)
+
+    // return combineDatasets(builderV1.build(), builderV2.build(), builderV3.build())
+    return builderV1.build()
+}
+
+private fun Model.addCommonDatasetValuesToBuilder(
+    recordResource: Resource,
+    datasetResource: Resource,
+    builder: Dataset.Builder
+) {
+    val harvestMetaData = recordResource.extractHarvestMetaData()
+
+    builder.id = recordResource.singleObjectStatement(DCTerms.identifier)!!.string
+    builder.uri = datasetResource.uri
     builder.harvest = harvestMetaData
-    builder.catalog = dataset.extractCatalogData()
+    builder.catalog = datasetResource.extractCatalogData()
 
-    builder.isRelatedToTransportportal = containsTriple(dataset.uri, FDK.isRelatedToTransportportal.uri, true)
-    builder.isOpenData = containsTriple(dataset.uri, FDK.isOpenData.uri, true)
-    builder.isAuthoritative = containsTriple(dataset.uri, FDK.isAuthoritative.uri, true)
-
-    builder.addV11Values(dataset)
-
-    return builder
+    builder.isRelatedToTransportportal = containsTriple(datasetResource.uri, FDK.isRelatedToTransportportal.uri, true)
+    builder.isOpenData = containsTriple(datasetResource.uri, FDK.isOpenData.uri, true)
+    builder.isAuthoritative = containsTriple(datasetResource.uri, FDK.isAuthoritative.uri, true)
 }
