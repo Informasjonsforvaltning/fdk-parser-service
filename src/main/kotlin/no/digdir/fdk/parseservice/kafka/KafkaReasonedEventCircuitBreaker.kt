ae116a5cc14e9117e6e7916e704d764c6f901120
Nils Ove Tendenes <nils.ove@tendenes.com>
2025-10-09 12:13:39 +0200

    fix: handle incoming generic messages kafka

--- src/main/kotlin/no/digdir/fdk/parseservice/kafka/KafkaReasonedEventCircuitBreaker.kt
+++ src/main/kotlin/no/digdir/fdk/parseservice/kafka/KafkaReasonedEventCircuitBreaker.kt
@@ -2,15 +2,14 @@ package no.digdir.fdk.parseservice.kafka
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker
 import io.micrometer.core.instrument.Metrics
+import no.digdir.fdk.parseservice.handler.DatasetHandler
 import no.digdir.fdk.parseservice.model.RecoverableParseException
 import no.digdir.fdk.parseservice.model.UnrecoverableParseException
-import no.digdir.fdk.parseservice.handler.DatasetHandler
 import no.fdk.dataset.DatasetEvent
 import no.fdk.dataset.DatasetEventType
 import no.fdk.rdf.parse.RdfParseEvent
 import no.fdk.rdf.parse.RdfParseResourceType
-import org.apache.avro.specific.SpecificRecord
-import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.apache.avro.generic.GenericRecord
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
@@ -23,22 +22,46 @@ open class KafkaReasonedEventCircuitBreaker(
     private val producer: KafkaRdfParseEventProducer,
     private val datasetHandler: DatasetHandler
 ) {
-    @CircuitBreaker(name = "rdf-parse")
-    open fun process(record: ConsumerRecord<String, SpecificRecord>) {
-        LOGGER.debug("Received message - offset: " + record.offset())
-        val event = record.value()
-
-        val resourceType = when (event) {
-            is DatasetEvent -> RdfParseResourceType.DATASET
-            else -> throw UnrecoverableParseException("Unknown event type")
+    @CircuitBreaker(name = "rdf-parse-generic")
+    open fun processGeneric(event: GenericRecord) {
+        val type = runCatching { event.get("type") as String }.getOrNull()
+        val resourceType = when (type) {
+            DatasetEventType.DATASET_REASONED.name -> RdfParseResourceType.DATASET
+            else -> null
         }
 
-        try {
-            event.let {
-                if (it.type == DatasetEventType.DATASET_REASONED) {
-                    parseAndProduce(it.fdkId.toString(), it.graph.toString(), it.timestamp, resourceType)
-                }
+        val fdkId = runCatching { event.get("fdkId") as String }.getOrNull()
+        val graph = runCatching { event.get("graph") as String }.getOrNull()
+        val timestamp = runCatching { event.get("timestamp") as Long }.getOrNull()
+
+        if (fdkId != null && graph != null && timestamp != null && resourceType != null) {
+            handleRecord(fdkId, graph, timestamp, resourceType)
+        } else {
+            LOGGER.error("Unable to get required message values. fdkId: {}, graph: {}, timestamp: {}, type: {}", fdkId, graph, timestamp, resourceType)
+            throw UnrecoverableParseException("Unable to get required message values")
+        }
+    }
+
+    @CircuitBreaker(name = "rdf-parse-dataset")
+    open fun processDataset(event: DatasetEvent) {
+        val type = runCatching { event.type }.getOrNull()
+        if (type == DatasetEventType.DATASET_REASONED) {
+            val fdkId = runCatching { event.fdkId.toString() }.getOrNull()
+            val graph = runCatching { event.graph.toString() }.getOrNull()
+            val timestamp = runCatching { event.timestamp }.getOrNull()
+
+            if (fdkId != null && graph != null && timestamp != null) {
+                handleRecord(fdkId, graph, timestamp, RdfParseResourceType.DATASET)
+            } else {
+                LOGGER.error("Unable to get required dataset message values. fdkId: {}, graph: {}, timestamp: {}", fdkId, graph, timestamp)
+                throw UnrecoverableParseException("Unable to get required dataset message values")
             }
+        }
+    }
+
+    private fun handleRecord(fdkId: String, graph: String, timestamp: Long, resourceType: RdfParseResourceType) {
+        try {
+            parseAndProduce(fdkId, graph, timestamp, resourceType)
         } catch (e: RecoverableParseException) {
             LOGGER.warn("Recoverable parsing error: " + e.message)
             Metrics.counter(
