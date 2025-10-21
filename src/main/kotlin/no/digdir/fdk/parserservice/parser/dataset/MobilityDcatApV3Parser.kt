ed4ab1be88931ad33f055e0117975cea64db2721
Nils Ove Tendenes <nils.ove@tendenes.com>
2025-10-21 15:15:34 +0200

    feat: support temporal, quality and qualified attributions for mobility

--- src/main/kotlin/no/digdir/fdk/parserservice/parser/dataset/MobilityDcatApV3Parser.kt
+++ src/main/kotlin/no/digdir/fdk/parserservice/parser/dataset/MobilityDcatApV3Parser.kt
@@ -2,14 +2,19 @@ package no.digdir.fdk.parserservice.parser.dataset
 
 import no.digdir.fdk.model.dataset.Dataset
 import no.digdir.fdk.parserservice.extract.containsTriple
+import no.digdir.fdk.parserservice.extract.dataset.extractListOfQualifiedAttributions
+import no.digdir.fdk.parserservice.extract.dataset.extractQualityAnnotation
+import no.digdir.fdk.parserservice.extract.extractListOfTemporal
 import no.digdir.fdk.parserservice.extract.fdk.addFdkData
 import no.digdir.fdk.parserservice.extract.fdk.fdkRecord
 import no.digdir.fdk.parserservice.extract.fdk.resourceOfIRI
 import no.digdir.fdk.parserservice.model.LanguageCodes
 import no.digdir.fdk.parserservice.model.NoAcceptableTypesException
+import no.digdir.fdk.parserservice.vocabulary.DQVISO
 import org.apache.jena.rdf.model.Model
 import org.apache.jena.rdf.model.Resource
 import org.apache.jena.vocabulary.DCAT
+import org.apache.jena.vocabulary.DCTerms
 import org.apache.jena.vocabulary.RDF
 import org.springframework.stereotype.Component
 import java.net.URI
@@ -44,16 +49,19 @@ class MobilityDcatApV3Parser() : BaseDatasetParser() {
 
         builder.addCommonDatasetValues(datasetResource)
 
-        builder.setTemporal(null)
+        builder.setTemporal(datasetResource.extractListOfTemporal(DCTerms.temporal, DCAT.startDate, DCAT.endDate))
+
         builder.setDistribution(null)
         builder.setSample(null)
 
-        builder.setHasRelevanceAnnotation(null)
-        builder.setHasCurrentnessAnnotation(null)
-        builder.setHasCompletenessAnnotation(null)
-        builder.setHasAvailabilityAnnotation(null)
-        builder.setHasAccuracyAnnotation(null)
-        builder.setQualifiedAttributions(null)
+        builder.setHasRelevanceAnnotation(datasetResource.extractQualityAnnotation(DQVISO.Relevance))
+        builder.setHasCurrentnessAnnotation(datasetResource.extractQualityAnnotation(DQVISO.Currentness))
+        builder.setHasCompletenessAnnotation(datasetResource.extractQualityAnnotation(DQVISO.Completeness))
+        builder.setHasAvailabilityAnnotation(datasetResource.extractQualityAnnotation(DQVISO.Availability))
+        builder.setHasAccuracyAnnotation(datasetResource.extractQualityAnnotation(DQVISO.Accuracy))
+        builder.setQualifiedAttributions(datasetResource.extractListOfQualifiedAttributions())
+
+        // The following properties are not implemented in mobilityDCAT-AP v3.0.0
         builder.setInSeries(null)
         builder.setDatasetsInSeries(null)
         builder.setLast(null)
