edf3675ed15191f11315a10ed07ca2e340066b93
Jeff Reiffers <jeffreiffers@gmail.com>
2025-12-11 18:03:05 +0100

    feat: produce harvest events

--- src/main/kotlin/no/digdir/fdk/parserservice/configuration/KafkaProducerConfig.kt
+++ src/main/kotlin/no/digdir/fdk/parserservice/configuration/KafkaProducerConfig.kt
@@ -1,5 +1,6 @@
 package no.digdir.fdk.parserservice.configuration
 
+import no.fdk.harvest.HarvestEvent
 import no.fdk.rdf.parse.RdfParseEvent
 import org.springframework.context.annotation.Bean
 import org.springframework.kafka.core.KafkaTemplate
@@ -8,5 +9,9 @@ import org.springframework.kafka.core.ProducerFactory
 open class KafkaProducerConfig {
     @Bean
     open fun kafkaTemplate(producerFactory: ProducerFactory<String, RdfParseEvent>): KafkaTemplate<String, RdfParseEvent> =
+        KafkaTemplate(producerFactory)
+
+    @Bean
+    open fun harvestEventKafkaTemplate(producerFactory: ProducerFactory<String, HarvestEvent>): KafkaTemplate<String, HarvestEvent> =
         KafkaTemplate(producerFactory)
 }
